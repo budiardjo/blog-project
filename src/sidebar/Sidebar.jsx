59a7ab60f1d7b7248aac5a9c6cea28fad6b68f0b
widjoyono <71638181+widjoyono@users.noreply.github.com>
2021-07-28 11:41:36 +0700

    adding sidebar

---
+++ src/sidebar/Sidebar.jsx
@@ -0,0 +1,40 @@
+import "./sidebar.css"
+
+export default function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebarItem">
+                <span className="sidebarTitle">ABOUT ME</span>
+                <img 
+                    src="https://ik.imagekit.io/fndn595ujcn/bruno-adam-9n-WKWQZrno-unsplash__1__JL6dYwPNs.jpg?updatedAt=1627445073148"
+                    alt=""
+                />
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas harum repudiandae eaque, tenetur eligendi ratione! Quisquam in, velit qui doloremque delectus labore explicabo iste odit expedita aut nulla ab facere.
+                </p>
+            </div>
+            <div className="sidebarItem">
+                <span className="sidebarTitle">
+                    CATEGORIES
+                </span>
+                <ul className="sidebarList">
+                    <li className="sidebarListItem">Life</li>
+                    <li className="sidebarListItem">Music</li>
+                    <li className="sidebarListItem">Style</li>
+                    <li className="sidebarListItem">Sport</li>
+                    <li className="sidebarListItem">Tech</li>
+                    <li className="sidebarListItem">Cinema</li>        
+                </ul>
+            </div>
+            <div className="sidebarItem">
+                <span className="sidebarTitle"></span>
+                <div className="sidebarSocial">
+                <i className="sidebarIcon fab fa-facebook-square"></i>
+            <i className="sidebarIcon fab fa-twitter-square"></i>
+            <i className="sidebarIcon fab fa-pinterest-square"></i>
+            <i className="sidebarIcon fab fa-instagram-square"></i>
+                </div>
+            </div>
+        </div>
+    )
+}
