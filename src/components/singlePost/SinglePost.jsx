1039fde88ffe76d1e8a0699d28cbfc638baf0db0
widjoyono <71638181+widjoyono@users.noreply.github.com>
2021-07-28 14:53:01 +0700

    new components

---
+++ src/components/singlePost/SinglePost.jsx
@@ -0,0 +1,10 @@
+import "./singlePost.css"
+
+export default function SinglePost() {
+    return (
+        <div clasName="singlePost">
+            single post
+            
+        </div>
+    )
+}
