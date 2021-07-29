5fab5d9ee18ce7d891ff9989e091db0e4bd6d491
widjoyono <71638181+widjoyono@users.noreply.github.com>
2021-07-29 14:13:06 +0700

    adding new post component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import TopBar from "./components/topbar/TopBar";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
--- src/pages/write/Write.jsx
+++ src/pages/write/Write.jsx
@@ -3,7 +3,22 @@ import "./write.css"
 export default function Write() {
     return (
         <div className="write">
-            writing this though journey
+            <img 
+            className="writeImg"
+            src="https://ik.imagekit.io/fndn595ujcn/default-image.jpg?updatedAt=1626507700303" alt="" />
+            <form className="writeForm">
+                <div className="writeFormGroup">
+                    <label className="fileInput">
+                        <i className="writeIcon fas fa-plus"></i>
+                    </label>
+                    <input type="file" id="fileInput" style={{display:"none"}} />
+                    <input type="text" placeholder="Title" className="writeInput" autoFocus={true}/>
+                </div>
+                <div className="writeFormGroup">
+                    <textarea placeholder="Tell your story..." type="text" className="writeInput writeText"></textarea>
+                </div>
+                <button className="writeSubmit">Publish</button>
+            </form>
         </div>
     )
 }
