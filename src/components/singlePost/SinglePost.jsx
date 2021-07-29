4f2154ea7953b45cdd83fa707d50b6ef7062264a
widjoyono <71638181+widjoyono@users.noreply.github.com>
2021-07-29 10:38:26 +0700

    single post done

--- src/components/singlePost/SinglePost.jsx
+++ src/components/singlePost/SinglePost.jsx
@@ -3,7 +3,28 @@ import "./singlePost.css"
 export default function SinglePost() {
     return (
         <div className="singlePost">
-            single post
+            <div className="singlePostWrapper">
+                <img
+                    className="singlePostImg" 
+                    src="https://ik.imagekit.io/fndn595ujcn/jimmy-dean-Yn0l7uwBrpw-unsplash_uO0zX0G3Y.jpg?updatedAt=1627528169745" 
+                    alt="" 
+                />
+                <h1 className="singlePostTitle">lorem ipsum dolor sit amet
+                    <div className="singlePostEdit">
+                        <i className="singlePostIcon far fa-edit"></i>
+                        <i className="singlePostIcon far fa-trash-alt"></i>
+                    </div>
+                
+                
+                </h1>
+            <div className="singlePostInfo">
+
+                <span className="singlePostAuthor">Author: <b>widjoyono</b></span>
+                <span className="singlePostDate">1 hour ago</span>
+            </div>
+            <p className="singlePostDesc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita perspiciatis tempore iusto adipisci sequi, consequatur nulla exercitationem sint dignissimos alias architecto! Magni culpa vel perferendis sed eum. Illo, mollitia beatae!
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita perspiciatis tempore iusto adipisci sequi, consequatur nulla exercitationem sint dignissimos alias architecto! Magni culpa vel perferendis sed eum. Illo, mollitia beatae! </p>
+            </div>
             
         </div>
     )
