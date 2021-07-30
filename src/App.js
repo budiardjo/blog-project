449d1318c556f254a014dd3ab80a679b6dd64e4f
widjoyono <71638181+widjoyono@users.noreply.github.com>
2021-07-30 11:36:41 +0700

    adding register and login page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import TopBar from "./components/topbar/TopBar";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 import Setting from "./pages/settings/Settings";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
@@ -11,7 +12,7 @@ function App() {
   return (
     <>
     <TopBar/>
-    <Login/>
+    <Register/>
     </>
   );
 }
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -3,11 +3,16 @@ import "./login.css"
 export default function Login() {
     return (
         <div className="login">
+            <span className="loginTitle">Login</span>
             <form className="loginForm">
                 <label>Email</label>
-                <input type="text" placeholder="Enter your email....." />
+                <input type="text" 
+                className="loginInput"
+                placeholder="Enter your email....." />
                 <label>Password</label>
-                <input type="password" placeholder="Enter your pass....." />
+                <input type="password" 
+                className="loginInput"
+                placeholder="Enter your pass....." />
                 <button className="loginButton">Login</button>
             </form>
             <button className="loginRegisterButton">Register</button>
